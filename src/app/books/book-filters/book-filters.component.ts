e0361ad18e3abc000ca9e10c096bbc990869ba9d
pranay_teja <s.pranayteja@gmail.com>
2021-02-27 19:02:43 +0530

    add search field in filters

--- src/app/books/book-filters/book-filters.component.ts
+++ src/app/books/book-filters/book-filters.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { tap, filter, debounceTime } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-book-filters',
   templateUrl: './book-filters.component.html',
-  styleUrls: ['./book-filters.component.css']
+  styleUrls: ['./book-filters.component.css'],
 })
-export class BookFiltersComponent implements OnInit {
+export class BookFiltersComponent implements OnInit, OnDestroy {
+  subscriptions: Subscription = new Subscription();
+  bookFilters: FormGroup = this.formBuilder.group({
+    title_contains: this.formBuilder.control(''),
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    let searchByTitleSubscription$ = this.bookFilters
+      .get('title_contains')
+      .valueChanges.pipe(
+        filter((val) => val !== ''),
+        debounceTime(1200),
+        tap(() => this.applyFilters())
+      )
+      .subscribe((data) => console.log(data));
+    this.subscriptions.add(searchByTitleSubscription$);
   }
 
+  applyFilters() {
+    console.log(this.bookFilters.getRawValue());
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 }
